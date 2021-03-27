1f779b3e1857ce78501169ca5c476908371f49e7
Real-cmd <sarderreal00@gmail.com>
2021-03-27 13:26:27 +0600

    initial -plugin development

---
+++ admego.php
@@ -0,0 +1,27 @@
+<?php
+/*
+@package    Admego
+*/
+
+/*
+Plugin Name:Admego
+Plugin URI:http://mysizzlers.co.uk
+Description: Hello This is my first plugin and This program is free software. You can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+version:1.0.0
+Author:Mithu Khan
+Author URI:http://mysizzlers.co.uk/author
+License:GPLv2 or later
+Text Domain:Admego domain
+*/
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
