e64906ed17c67c6e0e275be08cd4c8e364f0bb3e
Real-cmd <sarderreal00@gmail.com>
2021-03-28 17:30:35 +0600

    Project with composer-autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '02489142786903716414ae33ea5cb21e8947401a',
+    'name' => 'home/admego',
+  ),
+  'versions' => 
+  array (
+    'home/admego' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '02489142786903716414ae33ea5cb21e8947401a',
+    ),
+  ),
+);
